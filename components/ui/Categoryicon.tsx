55cc0b73edf9d7e04667d29dd74053088479b513
Carolina Martinez <carolinamtz091@gmail.com>
2024-06-03 07:34:25 -0600

    Agregando imagenes

--- components/ui/Categoryicon.tsx
+++ components/ui/Categoryicon.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image"
+import Link from "next/link"
 import { Category } from "@prisma/client"
+
 
 type CategoryIconProps = {
     category: Category
@@ -6,6 +9,21 @@ type CategoryIconProps = {
 
 export default function Categoryicon({category} : CategoryIconProps) {
   return (
-    <div>{category.name}</div>
+    <div
+    className={` flex items-center gap-4 w-full border-t border-gray-200 p-3
+    last-of-type:border-b`}
+    >
+      <div className="w-16 h-16 relative">
+      <Image
+      fill
+       src={`/icon_${category.slug}.svg`}
+       alt="Imagen Categoria"/>
+       </div>
+
+       <Link
+          className="text-xl font-bold"
+          href={`/order/${category.slug}`}
+       >{category.name}</Link>
+    </div>
   )
 }
